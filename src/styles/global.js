c02c846f774ada20f751738da10e574a463237af
Cesar Martinez <cmarvarc@hotmail.com>
2022-08-09 14:48:00 -0500

    Created reset css js doc and global const with body font as Poppins

---
+++ src/styles/global.js
@@ -0,0 +1,27 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { fonts } from './fonts';
+
+export const reset = css`
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+`;
+
+export const global = css`
+  body {
+    font-family: ${fonts.primary}
+  }
+`
+
