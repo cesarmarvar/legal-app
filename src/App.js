2bb429c1a34dc9499477055c8cbf29fa5f3bd63d
Cesar Martinez <cmarvarc@hotmail.com>
2022-08-13 16:29:09 -0500

    Added footer to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <>
       <Header />
       <h1>Legal app!</h1>
+      <Footer />
     </>
   );
 }
