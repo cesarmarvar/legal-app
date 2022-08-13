4f07a55505a5a43b6eb494845b636d703291346a
Cesar Martinez <cmarvarc@hotmail.com>
2022-08-13 16:28:12 -0500

    Created utils doc to stash reusable styled elements

---
+++ src/utils.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const FlexRow = styled.div`
+  display: flex;
+`
+
+export const FlexColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+`
