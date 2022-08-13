edb38266eba18bf15d986b300485f5fa9b357fed
Cesar Martinez <cmarvarc@hotmail.com>
2022-08-13 15:51:02 -0500

    Created list of  colors from figma design in an object.

---
+++ src/styles/colors.js
@@ -0,0 +1,16 @@
+export const colors = {
+  blue: {
+    regular: "#3C4E8D",
+    opaque_1: "#3C4E8DCF",
+    opaque_2: "#3C4E8D87"
+  },
+  orange: {
+    regular: "#DB821AB5",
+    stars: "#DB821AD6"
+  },
+  gray: {
+    regular: "#D9D9D97A"
+  }, 
+  white: "#FFFFFF", 
+  black: "#000000"
+}
