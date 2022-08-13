b268256746bc5bc96cb4a670088dce3233169b13
Cesar Martinez <cmarvarc@hotmail.com>
2022-08-13 17:02:46 -0500

    Created basic version of single input component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
+import Input from "./components/input/input";
 
 function App() {
   return (
     <>
       <Header />
       <h1>Legal app!</h1>
+      <Input type="text"/>
       <Footer />
     </>
   );
