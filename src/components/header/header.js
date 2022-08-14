5f0ade435c8deb8b86fc02c990d8ff6537a3b93b
Cesar Martinez <cmarvarc@hotmail.com>
2022-08-13 19:24:53 -0500

    Fixed header to align menu button accordingly

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,22 +1,19 @@
 import { BiMenu } from "react-icons/bi";
 import styled from "@emotion/styled";
 
-const FlexRow = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 52.5%; // =============================== temp fix to display title on middle
-`
+
 const Title = styled.h1`
   margin: 0px;
   color: rgba(28, 32, 119, 0.91);
   font-size: 32px;
   font-weight: 600;
+  text-align: center;
 `
 const Menu = styled.button`
   background: none;
   border: none;
   cursor: pointer;
+  position: absolute;
 `
 //border: "2px solid red"
 function Header() {
@@ -26,12 +23,12 @@ function Header() {
   }
 
   return(
-    <FlexRow style={{margin: "1rem"}}>
+    <header style={{padding: "1rem"}}>
       <Menu >
         <BiMenu onClick={handleMenuClick} style={{marginTop: "6px"}} size="35px"/>
       </Menu>
       <Title>Legal</Title>
-    </FlexRow>
+    </header>
   )
 }
 
