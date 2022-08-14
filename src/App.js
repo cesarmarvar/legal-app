20d2a8281f9c920b09df0e8559927e3ee2922c19
Cesar Martinez <cmarvarc@hotmail.com>
2022-08-13 20:28:17 -0500

    Formatted inputs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import Header from "./components/header/header";
-import Footer from "./components/footer/footer";
-import Input from "./components/input/input";
+import MainPage from "./pages/main";
 
 function App() {
   return (
     <>
-      <Header />
-      <h1>Legal app!</h1>
-      <Input type="text"/>
-      <Footer />
+      <MainPage />
     </>
   );
 }
--- src/pages/main.js
+++ src/pages/main.js
@@ -18,8 +18,14 @@ function MainPage() {
       <h3 style={{color: `${colors.blue.regular}`, marginTop: "30px", marginBottom: "30px"}}>Encuentra a tu representante</h3>
       <form>
         <FlexColumn style={{gap: "20px"}}>
-          <Input placeholder="Legal issue or Lawyer name" type="query"/>
-          <Input placeholder="Location" type="query"/>
+          <Input 
+            placeholder="Legal issue or Lawyer name" 
+            type="query"
+          />
+          <Input 
+            placeholder="Location" 
+            type="query"
+          />
           <Button size="tall" type="primary">get started</Button>
         </FlexColumn>
       </form>
