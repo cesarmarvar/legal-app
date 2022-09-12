00d92b45303edb40cdacfb2f9355bf0a9f39826f
Cesar Martinez <cmarvarc@hotmail.com>
2022-09-12 18:10:46 -0500

    Created create question page. Functional.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 // import { LawyerDetailPage } from "./pages/lawyer-detail/lawyer-detail";
 // import { LawyersPage } from "./pages/lawyers";
 // import MainPage from "./pages/main";
-
 // import StarRating from "./components/textarea/star-rating/star-rating";
-
-import { CreateReview } from "./pages/create-review/create-review-page";
+// import { CreateReview } from "./pages/create-review/create-review-page";
+import CreateQuestionPage from "./pages/create-question/create-question-page";
 
 function App() {
   return (
@@ -12,8 +11,8 @@ function App() {
       {/* <MainPage /> */}
       {/* <LawyersPage /> */}
       {/* <LawyerDetailPage /> */}
-      <CreateReview />
-      {/* <StarRating /> */}
+      {/* <CreateReview /> */}
+      <CreateQuestionPage />
     </>
   );
 }
---
+++ src/pages/create-question/create-question-page.js
@@ -0,0 +1,108 @@
+import { useState } from "react";
+import { Button } from "../../components/button/button";
+import { Input } from "../../components/input/input";
+import { Textarea } from "../../components/textarea/textarea";
+import { createQuestion } from "../../services/questions-services";
+import { DivisionLine, FlexRow, FlexColumn, MainContainer } from "../../utils"
+import { InputContainer, Label } from "../create-review/styles";
+
+
+function CreateQuestionPage() {
+
+  const [ questionData, setQuestionData ] = useState({
+    question: "",
+    explanation: "",
+    confirmation: "",
+    user_id: 1
+  })
+
+  const { question, explanation } = questionData
+
+  function handleChange(e) {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setQuestionData({...questionData, [name]: value});
+  }
+
+  function handleConfirmationClick(option){
+    setQuestionData({...questionData, confirmation: option})
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    console.log(questionData);
+    createQuestion(questionData)
+    .then(console.log)
+    .catch(console.log)
+  }
+
+  return (
+  <MainContainer>
+    <h1>Ask a Lawyer</h1>
+    <DivisionLine />
+    <main style={{marginTop: "1.5rem", marginBottom: "1.5rem"}}>
+      <form>
+        <FlexColumn style={{gap: "1rem"}}>
+          <Input 
+            id="question"
+            name="question"
+            label="Ask your question"
+            value={question}
+            onChange={handleChange}
+          />
+          <Textarea 
+            id="explanation"
+            name="explanation"
+            label="Explain your situation"
+            value={explanation}
+            onChange={handleChange}
+          />
+          <InputContainer>
+            <Label htmlFor="context">Was this a consultation or did you hire this attorney?</Label>
+            <FlexRow style={{gap: "2rem"}}>
+              <FlexRow style={{gap: "0.5rem"}}>
+                <input 
+                  type="radio" 
+                  id="Yes" 
+                  name="confirmation" 
+                  value="Yes"
+                  onClick={() => handleConfirmationClick("yes")}
+                />
+                <label htmlFor="Consulted">Yes</label>
+              </FlexRow>
+              <FlexRow style={{gap: "0.5rem"}}>
+                <input 
+                  type="radio" 
+                  id="No" 
+                  name="confirmation" 
+                  value="No"
+                  onClick={() => handleConfirmationClick("no")}
+                />
+                <label htmlFor="No">No</label>
+              </FlexRow>
+              <FlexRow style={{gap: "0.5rem"}}>
+                <input 
+                  type="radio" 
+                  id="Maybe" 
+                  name="confirmation" 
+                  value="Maybe"
+                  onClick={() => handleConfirmationClick("maybe")}
+                />
+                <label htmlFor="Maybe">Maybe</label>
+              </FlexRow>
+            </FlexRow>
+          </InputContainer>
+          <Button 
+            type="secondary" 
+            size="wide" 
+            style={{margin: "0 auto"}}
+            onClick={handleSubmit}
+          >Submit</Button>
+        </FlexColumn>
+      </form>
+    </main>
+  </MainContainer>
+  )
+};
+
+export default CreateQuestionPage;
