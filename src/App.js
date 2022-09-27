53da3424dca976bbef207a99c3c1957b87586e6d
Cesar Martinez <cmarvarc@hotmail.com>
2022-09-27 11:46:57 -0500

    Added route for signup page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import MainPage from "./pages/main";
 import { CreateReview } from "./pages/create-review/create-review-page";
 import CreateQuestionPage from "./pages/create-question/create-question-page";
 import { Routes, Route } from "react-router-dom";
+import IndexQuestionsPage from "./pages/index questions/index-questions";
+import ShowQuestion from "./pages/show question/show-question";
+import SignupPage from "./pages/signup/login pages/signup";
 
 function App() {
   return (
@@ -13,7 +16,10 @@ function App() {
         <Route path="lawyers" element={<LawyersPage />} />
         <Route path="lawyers/:id" element={<LawyerDetailPage />} />
         <Route path="reviews/new/:id" element={<CreateReview />} />
+        <Route path="questions" element={<IndexQuestionsPage />} />
         <Route path="questions/new/" element={<CreateQuestionPage />} />
+        <Route path="questions/:id" element={<ShowQuestion />} />
+        <Route path="signup/" element={<SignupPage />} />
       </Routes>
     </>
   );
