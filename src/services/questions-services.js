d06e661775ce8f742aacf926ad0077a96d13fa6f
Cesar Martinez <cmarvarc@hotmail.com>
2022-09-27 11:20:43 -0500

    Show question function

--- src/services/questions-services.js
+++ src/services/questions-services.js
@@ -1,5 +1,21 @@
 const BASE_URL = "https://legalapp-0822.herokuapp.com/"
 
+export async function getQuestions() {
+  const response = await fetch(`${BASE_URL}questions`, {
+    method: "GET"
+  })
+  const data = response.json();
+  return data
+}
+
+export async function showQuestion(id) {
+  const response = await fetch(`${BASE_URL}question/${id}`, {
+    method: "GET"
+  })
+  const data = response.json();
+  return data
+}
+
 export async function createQuestion(formData) {
   const response = await fetch(`${BASE_URL}questions/new`, {
     method: "POST",
